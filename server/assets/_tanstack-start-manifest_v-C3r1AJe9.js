a298c4f38e7a486820dba02c74be548f2722256b
Leticia <0001159429@senaimgaluno.com.br>
2026-04-15 17:48:55 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-C3r1AJe9.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Letícia/Desktop/LandingPageGP/projeto-sidi/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/RevolucaoVerde-landing-page/assets/index-u7jACP14.js"] }, "/": { filePath: "C:/Users/Letícia/Desktop/LandingPageGP/projeto-sidi/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/RevolucaoVerde-landing-page/assets/index-OvDzmCEo.js"] } }, clientEntry: "/RevolucaoVerde-landing-page/assets/index-u7jACP14.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,9 @@
+import { _, $ } from "./assets/worker-entry-B37J1bus.js";
+import "node:events";
+import "node:async_hooks";
+import "node:stream/web";
+import "node:stream";
+export {
+  _ as createServerEntry,
+  $ as default
+};
